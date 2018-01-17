440a3e407d7a0ea3f4c9e729b641254ee7f151c8
Autumn <autumn2@sting.graceland.edu>
2018-01-17 11:30:29 -0600

    Fix

---
+++ RacingTurtles copy.py
@@ -0,0 +1,11 @@
+#Racing turtles
+
+from turtle import * #star allows us to not need .turtle
+
+mack = Turtle()
+
+#mack.forward(100)
+#mack.left(30)
+#mack.forward(100)
+
+#Create a simulation that predicts what turtles will win a race
--- RacingTurtles.py
+++
@@ -1,9 +0,0 @@
-#Racing turtles
-
-from turtle import * #star allows us to not need .turtle
-
-mack = Turtle()
-
-mack.forward(100)
-mack.left(30)
-mack.forward(100)
