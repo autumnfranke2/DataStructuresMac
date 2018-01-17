5d545511760dca94fdd85b3afb2734c14e4162ff
Autumn <autumn2@sting.graceland.edu>
2018-01-17 11:41:33 -0600

    class RacingTurtle

--- RacingTurtles copy.py
+++ RacingTurtles copy.py
@@ -9,3 +9,13 @@ mack = Turtle()
 #mack.forward(100)
 
 #Create a simulation that predicts what turtles will win a race
+#Racerone - average turtle
+#Eugene - runs 15% faster than an average turtle, but must wait .5 second before he can turn
+#Blaze -retired, but runs at 40% increased speed
+
+class RacingTurtle:
+
+    def __inti__ (self, speed, turnDelay): #Constructor *always include self
+        self.turt = Turtle()
+        self.speed = 20 * (1 + (speed/100))
+        self.turnDelay = 0 + turnDelay
