784e672b4f85237ade0f20b006538d2a1dc37ff5
Autumn <autumn2@sting.graceland.edu>
2018-01-21 11:38:46 -0600

    FizzBuzz

---
+++ FizzBuzz.py
@@ -0,0 +1,19 @@
+# FizzBuzz
+#Autumn (worked with Mandy)
+
+import math
+
+# divisble by 3 = fizz
+#divisable
+
+class FizzBuzz
+
+    def __init__(self,x,y) #Decisions, decisions
+
+    def Fizz(self,x):
+        int x = 0
+        while(x < 100){
+            x = ++;
+            if(){
+                }
+            }
