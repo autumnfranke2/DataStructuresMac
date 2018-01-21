2c055a60b30c9885f0231397fc8bd16d6e8e7561
Autumn <autumn2@sting.graceland.edu>
2018-01-21 11:49:34 -0600

    Attempt 1

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,17 +3,26 @@
 
 import math
 
-# divisble by 3 = fizz
-#divisable
+# divisable by 3 = fizz
+# divisable by 5 = buzz
 
 class FizzBuzz
 
     def __init__(self,x,y) #Decisions, decisions
 
-    def Fizz(self,x):
+    def FizzBuzz(self,x,y):
         int x = 0
         while(x < 100){
             x = ++;
-            if(){
+            if(x % 3 = 0, x % 5 = 0){
+                print(x,"FizzBuzz")
                 }
+            elif(x % 53= 0){
+                print(x,"Fizz")
+                }
+            elif(x % 5 = 0){
+                print(x,"Buzz")
+                }
+            else(){
+                print(x)}
             }
