93023ffb88fbddebe75ee59adb6f08e9d34784ed
Autumn <autumn2@sting.graceland.edu>
2018-01-28 21:57:01 -0600

    First draft

---
+++ Simulation.py
@@ -0,0 +1,82 @@
+#Autumn Franke (worked with Mandy)
+#Simulation
+# 1-28-2018
+
+
+# each cycle = 1 minute
+
+
+class WaitingRoom:
+
+    def __init__(self, time, patient):
+        self.time = 0
+        self.patient = 0
+
+    def NewPatient(self):
+        self.patient += 1
+
+    def WaitingPatient(self, time):
+        if ExamRoom == 0:
+            WaitingPatient += NewPatient
+
+
+
+class ExamRoom:
+
+    def __init__(self, time, patient, rooms, physician):
+        self.rooms = 6
+        self.time = # random between 15 - 20
+        self.patient = patient <= 1
+        self.physician = 1
+
+    def Rooms(self):
+        if Rooms is empty:
+            Rooms += WaitingPatient
+        # 1 patient per room
+        # physician enters room and starts time
+        self.time()
+
+
+class Patient:
+
+    def __init__(self):
+        self.patient(1)
+
+class Nurse:
+
+    def __init__(self):
+        # not sure what to do here
+
+
+    def NurseCycle(self, patient):
+        for Nurse in Rooms(1,7):
+            
+            if WaitingRoom != 0:
+
+                if Rooms == 0:
+                    # leads patient to exam room
+                else:
+                    #patient stays in waiting room
+            else:
+                return ('waiting room is empty')
+        
+
+class Physician:
+    def __init__(self):
+        # not sure what to do here
+
+    def PhysicianCycle():
+
+        for Physician in Rooms(1,7):
+            if Rooms != 0:
+                #enters room
+                self.time #Random between 15-20
+                #leaves room
+                #return to begining of loop
+            else:
+                # check the next room
+            
+
+    
+
+    
