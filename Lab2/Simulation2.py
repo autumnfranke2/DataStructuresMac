80a6216e5530b719fed0c3f1d89d521ca304ebc6
Autumn <autumn2@sting.graceland.edu>
2018-02-03 15:29:46 -0600

    attempt1

--- Lab2/Simulation2.py
+++ Lab2/Simulation2.py
@@ -18,14 +18,41 @@ def callNurse():
     triageRoom.append(waitingRoom.pop(0))
     sort(triageRoom, key = patient.triageNumber)
 
+    """nurse moves patient from triage room to exam room"""
+    ExamRoom()
+
+
 class Patient:
     def __init__(self):
         self.triageNumber = random.randint(0,100)
         self.name = names[random.randint(0,len(names)-1)] \
                     + " " + names[random.randint(0,len(names)-1)]
-##        self.arrivalTime = time
-##        self.treatmentTime = random.randrange(15,20)
+        self.arrivalTime = time.clock()
+        self.treatmentTime = random.randrange(15,20)
+
+    def enterWaitingRoom(self):
+        waitingRoom.append(self.name)
 
     def exit(self):
-        pass
-        #remove patient from simulation
+        examRoom.remove(self.name)
+
+
+
+def ExamRoom(self):
+    if examRoom <= 6:
+        examRoom.append(triageRoom.pop(0))
+    else:
+        null
+
+   
+def DocOffice():
+    
+    PatientName = self.name()
+    PatientNumber = self.triageNumber()
+    PatientArrival = self.arrivalTime()   
+    time.clock()
+    print(PatientName, PatientNumber, PatientArrival)
+
+
+    
+    
