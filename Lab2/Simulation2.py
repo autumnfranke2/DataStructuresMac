f6c2e25483b2d5c4b772a01e5042ff2c3f9627ce
Autumn <autumn2@sting.graceland.edu>
2018-02-04 21:15:09 -0600

    works

--- Lab2/Simulation2.py
+++ Lab2/Simulation2.py
@@ -1,6 +1,8 @@
 import random
+import time
+
 names = ["Joey", "Bobby", "Sueann", "Loretta", "Grant",\
-         "Genny", "Billy", "Tucker", "Cletus", "Hunter"\
+         "Genny", "Billy", "Tucker", "Cletus", "Hunter",\
          "Gunner", "Rose", "Amy", "Charlette", "Duke", \
          "Ricky", "Bo", "Luke", "Jesse"]
 
@@ -9,17 +11,9 @@ waitingRoom = []
 triageRoom = []
 
 examRoom = []
+
 examRoomSize = 6
 doctors = 6
-
-
-def callNurse():
-    """move patient from waiting room to triage room"""
-    triageRoom.append(waitingRoom.pop(0))
-    sort(triageRoom, key = patient.triageNumber)
-
-    """nurse moves patient from triage room to exam room"""
-    ExamRoom()
 
 
 class Patient:
@@ -29,30 +23,38 @@ class Patient:
                     + " " + names[random.randint(0,len(names)-1)]
         self.arrivalTime = time.clock()
         self.treatmentTime = random.randrange(15,20)
+        
 
-    def enterWaitingRoom(self):
-        waitingRoom.append(self.name)
-
-    def exit(self):
-        examRoom.remove(self.name)
+PatientInfo = Patient()
 
 
-
-def ExamRoom(self):
-    if examRoom <= 6:
-        examRoom.append(triageRoom.pop(0))
-    else:
-        null
-
-   
-def DocOffice():
+def callNurse():
+    from operator import itemgetter, attrgetter, methodcaller
+    """move patient from waiting room to triage room"""
     
-    PatientName = self.name()
-    PatientNumber = self.triageNumber()
-    PatientArrival = self.arrivalTime()   
-    time.clock()
-    print(PatientName, PatientNumber, PatientArrival)
+    triageRoom.append(waitingRoom.pop(0))
+    sorted(triageRoom, key = lambda x: x[2])
+    print("Triage Room:", triageRoom)
+    
+    """nurse moves patient from triage room to exam room"""
+    ExamRoom()
+def enterWaitingRoom():
+    waitingRoom.append((PatientInfo.name, PatientInfo.triageNumber,
+                        PatientInfo.arrivalTime))
+    print("Waiting Room:", waitingRoom)
+
+def exit():
+    examRoom.remove()
+
+def ExamRoom():
+        if len(examRoom) < 7 :
+            examRoom.append(triageRoom.pop(0))
+            print("Exam Room: ", examRoom)
+        else:
+            print("Triage Room:", triageRoom)
 
 
-    
-    
+time.clock()
+enterWaitingRoom()
+callNurse()
+
