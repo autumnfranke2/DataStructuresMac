3120b61a80303afde04a35ed1033a120270652d5
Autumn <autumn2@sting.graceland.edu>
2018-02-18 21:39:51 -0600

    part c added

--- Lab4/PartB.py
+++ Lab4/PartB.py
@@ -32,20 +32,41 @@ def fibRecursive(n):
 
 Recursion = ("recursion: ", fibRecursive(a), "runtime for recursion ", time.clock())
 
+#PartC
 
-nonRecursiveTime = nonRecursion[3:4]
-RecursiveTime = Recursion[3:4]
+def fibDynamic(n):
+    fibList = [0,1]
+    time.clock()
+    time.clock()
 
+    if(n <= 1):
+        return(fibList[n])
+    else:
+        fibList.append(fibList[n-1] + fibList[n-2])
+    return fibList[n]
+    
+
+Dynamic = ("dynamic: ", fibDynamic(a), "runtime for Dynamic ", time.clock())
 
 
 def timeComparison():
-    if nonRecursiveTime > RecursiveTime:
+    nonRecursiveTime = nonRecursion[3:4]
+    RecursiveTime = Recursion[3:4]
+    DynamicTime = Dynamic[3:4]
+    if (nonRecursiveTime > RecursiveTime and DynamicTime > RecursiveTime):
         return("Recursion was faster")
-    else:
+    elif (RecursiveTime > nonRecursiveTime and DynamicTime > nonRecursiveTime):
         return("non Recursion was faster")
-
+    else:
+        return("Dynamic programming was faster")
 
 
 print(nonRecursion)
 print(Recursion)
+print(Dynamic)
 print(timeComparison())
+
+
+
+
+
