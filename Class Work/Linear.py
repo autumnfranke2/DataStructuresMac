100d9ae7aa8b65b698ed5ebc9086640a2adabbae
Autumn <autumn2@sting.graceland.edu>
2018-03-24 12:44:44 -0500

    stuff

---
+++ Class Work/Linear.py
@@ -0,0 +1,40 @@
+#Linear Data Structure
+#class work
+
+
+
+class LinearItem:
+    def __init__(self):
+        self.val = value
+        self.next = None
+        self.prev = None
+
+    def setNext(self, item):
+        self.next = item
+    def setPrev(self, item):
+        self.prev = item
+
+    def getNext(self):
+        return self.next
+    def getPrev(self):
+        return self.previous
+    def getValue(self):
+        return self.val
+
+
+class Linear:
+    def __init__(self):
+        self.first = None
+        self.last = None
+        self.length = 0
+
+    def add(self, item):
+        newItem = LinearItem(item)
+        if self.length == 0:
+            self.first, self.last = newItem, newItem
+            self.length +=1
+        else:
+            self.last.setNext(newItem)
+            newItem.setPrev(self.last)
+            self.last = newItem
+            
